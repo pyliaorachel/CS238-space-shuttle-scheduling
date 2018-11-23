6fa3766a50087316e883188595427ac95a11e76e
zchen131 <44451451+zchen131@users.noreply.github.com>
2018-11-22 22:13:59 -0800

    Update q_learning.py

--- src/model/q_learning.py
+++ src/model/q_learning.py
@@ -1,2 +1,14 @@
+from qlearnpolicy import QLearnPolicy
+import time 
+import pandas as pd
+
+data = pd.read_csv('data.csv')
 def train(env, n_episodes):
-    pass
+    QL = QLearnPolicy(large_data)
+    start = time.time()
+    QL.Q_learning()
+    end = time.time()
+    print(end-start)
+    reward = QL.train()
+
+    return reward
