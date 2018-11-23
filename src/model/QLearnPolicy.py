28c145776a328ac56bc76c3b124cef522177c3c3
zchen131 <44451451+zchen131@users.noreply.github.com>
2018-11-22 22:08:15 -0800

    Create QLearnPolicy.py

---
+++ src/model/QLearnPolicy.py
@@ -0,0 +1,35 @@
+import numpy as np
+from tqdm import tqdm
+
+
+class LargePolicy(object):
+    def __init__(self, data):
+        self.data = data
+        self.no_st = 365*3
+        self.no_act = 365
+        self.Q = np.zeros((self.no_st, self.no_act))
+        self.policy = np.zeros(self.no_st)
+
+    def Q_learning(self):
+        for row in tqdm(self.data.iterrows(), total=len(self.data)):
+            s, a, r, sp,delay = row[1]
+            self.Q[s - 1, a - 1] = self.Q[s - 1, a - 1] + 0.1*(r + 0.95 * np.max(self.Q[sp - 1, a - 1]) - self.Q[s - 1, a - 1])
+
+        self._update_policy()
+
+    def _update_policy(self):
+        for s in tqdm(range(self.no_st)):
+            if not np.any(self.Q[s]):
+                self.policy[s] = np.random.randint(1, 366)
+            else:
+                self.policy[s] = np.argmax(self.Q[s]) + 1
+
+    def output_policy(self):
+        with open('large.policy', 'w+') as f:
+            f.writelines([str(int(x)) + '\n' for x in self.policy])
+
+    def train(self):
+      reward = 0
+      for s in tqdm(range(self.no_st)):
+        reward = reward+self.Q[s]
+      return sum(reward)
